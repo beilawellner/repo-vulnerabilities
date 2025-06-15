fdd6257683f06bf0f4a9056dd95a3f5fda08f37f
PolaDinitz <44160650+PolaDinitz@users.noreply.github.com>
2025-06-15 14:15:26 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -21,9 +21,6 @@ resource "aws_s3_bucket" "data" {
 }
 
 resource "aws_s3_bucket_object" "data_object" {
-  bucket = aws_s3_bucket.data.id
-  key    = "customer-master.xlsx"
-  source = "resources/customer-master.xlsx"
   tags = merge({
     Name        = "${local.resource_prefix.value}-customer-master"
     Environment = local.resource_prefix.value
